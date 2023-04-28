d3455efbfd0cd27e3a863e68dcdbd1d9cad455b0
enesozmert <en-es111@hotmail.com>
2023-04-28 20:10:34 +0300

    parse edit

--- inc/parser/Parser.hpp
+++ inc/parser/Parser.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <map>
+#include <iterator>
 #include "../file/File.hpp"
 
 class File;
@@ -17,8 +19,14 @@ class Parser
 
         std::string cleanString(std::string str);
         std::string trim(const std::string& str, const std::string& delimiters);
-        std::vector<std::string> splitString(const std::string& str, const char delimiter);
-        void parserLocationPath(const std::string &location);
+        std::vector<std::string> splitString(const std::string &str, const char delimiter);
+
         void parse(std::string &fileName);
+        void parserScope(const std::vector<std::string> &lines);
+
+        void parserLocationPath(const std::string &location);
+        void location(std::vector<std::string> tempScopes);
+        void server(std::vector<std::string> tempScopes);
+        void http(std::vector<std::string> tempScopes);
 };
 
--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,5 +1,7 @@
 #include "../inc/parser/Parser.hpp"
 
+typedef void (Parser::*method_function)(std::vector<std::string>);
+
 Parser::Parser(/* args */)
 {
 }
@@ -8,49 +10,127 @@ Parser::~Parser()
 {
 }
 
-std::string Parser::cleanString(std::string str) {
+std::string Parser::cleanString(std::string str)
+{
     std::string cleanedStr;
     const char *ch = str.c_str();
-    for (size_t i = 0; i < str.length(); i++) {
-        if (ch[i] != '\t' && ch[i] != '\n') {
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if (ch[i] != '\t' && ch[i] != '\n')
+        {
             cleanedStr += ch[i];
         }
     }
     return cleanedStr;
 }
 
-std::string Parser::trim(const std::string& str, const std::string& delimiters) {
+std::string Parser::trim(const std::string &str, const std::string &delimiters)
+{
     size_t first = str.find_first_not_of(delimiters);
-    if (first == std::string::npos) {
+    if (first == std::string::npos)
+    {
         return "";
     }
     size_t last = str.find_last_not_of(delimiters);
     return str.substr(first, (last - first + 1));
 }
 
-std::vector<std::string> Parser::splitString(const std::string& str, const char delimiter)
+std::vector<std::string> Parser::splitString(const std::string &str, const char delimiter)
 {
     std::vector<std::string> tokens;
-    std::string word;
-
     std::stringstream ss(str);
-    while (!ss.eof()) {
-        getline(ss, word, delimiter);
-        tokens.push_back(word);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter))
+    {
+        tokens.push_back(token);
     }
+
     return tokens;
 }
 
-// void Parser::parseLocation(std::vector<std::string> locations)
-// {
+void Parser::location(std::vector<std::string> tempScopes)
+{
+    // (void)tempScopes;
+    for (size_t i = 0; i < tempScopes.size(); i++)
+    {
+        std::cout << "Server ok parseLocation: " << tempScopes[i] << std::endl;
+    }
 
-// }
+    std::cout << "**********parseLocation" << std::endl;
+}
+
+void Parser::server(std::vector<std::string> tempScopes)
+{
+    (void)tempScopes;
+    std::cout << "**********parseServer" << std::endl;
+}
+
+void Parser::http(std::vector<std::string> tempScopes)
+{
+    (void)tempScopes;
+    std::cout << "**********parseHttp" << std::endl;
+}
 
 void Parser::parserLocationPath(const std::string &location)
 {
-    std::cout << "location: " << location << std::endl;
+    std::vector<std::string> locationPath = splitString(location, ' ');
+    std::cout << "locationPath: " << locationPath[1] << std::endl;
 }
 
+void Parser::parserScope(const std::vector<std::string> &lines)
+{
+    int i;
+    std::string scopeTexts[3];
+
+    i = -1;
+    scopeTexts[0] = "http";
+    scopeTexts[1] = "server";
+    scopeTexts[2] = "location";
+    method_function p[3] = {&Parser::http, &Parser::server, &Parser::location};
+    std::vector<std::string> tmpScopes;
+
+    for (size_t j = 0; j < 3; j++)
+    {
+        int inServerBlock = 0;
+        int lineCount = 0;
+        for (size_t i = 0; i < lines.size(); i++)
+        {
+            std::string line = lines[i];
+            std::string lineTrim = this->trim(line, " {");
+            if (lineTrim.find(scopeTexts[j]) != std::string::npos && line.find("{") != std::string::npos)
+            {
+                if (scopeTexts[j] == "location")
+                    this->parserLocationPath(lineTrim);
+                inServerBlock++;
+                // std::cout << "inServerBlock" << inServerBlock << std::endl;
+                continue;
+            }
+            if (line.find("}") != std::string::npos && inServerBlock > 0)
+            {
+                inServerBlock--;
+                continue;
+            }
+            if (lineTrim.find(scopeTexts[j]) == std::string::npos && inServerBlock > 0)
+            {
+                // server bloğundayız
+                tmpScopes.push_back(line);
+                // std::cout << "Server ok: " << line << std::endl;
+                lineCount++;
+                // burada yapılacak işlemler
+            }
+            else if (inServerBlock == 0)
+            {
+                // std::cout << "scopeTexts[j] " << scopeTexts[j] << "   Server no" << std::endl;
+                // server bloğunda değiliz
+                // burada yapılacak işlemler
+            }
+        }
+        (this->*p[j])(tmpScopes);
+        tmpScopes.clear();
+        tmpScopes.resize(0);
+    }
+}
 
 void Parser::parse(std::string &fileName)
 {
@@ -60,47 +140,14 @@ void Parser::parse(std::string &fileName)
     std::string fileCleanContents = cleanString(fileContents);
     std::vector<std::string> lines;
     std::string delimiter = "{};";
-    std::vector<std::string> locations;
     while ((pos = fileCleanContents.find_first_of(delimiter)) != std::string::npos)
     {
         std::string line = fileCleanContents.substr(0, pos + 1);
         lines.push_back(line);
         fileCleanContents.erase(0, pos + 1);
     }
+    parserScope(lines);
     file.close();
 
-    int inServerBlock = 0;
-    for (size_t i = 0; i < lines.size(); i++)
-    {
-        std::string line = lines[i];
-        if (line.find("{") != std::string::npos)
-        {
-            this->parserLocationPath(line);
-            std::string lineTrim = this->trim(line, " {");
-            if (lineTrim.find("location") != std::string::npos)
-            {
-                inServerBlock++;
-                continue;
-            }
-        }
-        if (line.find("}") != std::string::npos && inServerBlock)
-        {
-            inServerBlock--;
-            continue;
-        }
-        if (inServerBlock)
-        {
-            // server bloğundayız
-            locations.push_back(line);
-            std::cout << "Server ok: " << line << std::endl;
-            // burada yapılacak işlemler
-        }
-        else
-        {
-            std::cout << "Server no" << std::endl;
-            // server bloğunda değiliz
-            // burada yapılacak işlemler
-        }
-    }
-    // std::cout << fileContents << std::endl;
+    // std::cout << "lineCount" << lineCount << std::endl;
 }
