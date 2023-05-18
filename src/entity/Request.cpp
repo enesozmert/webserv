8fa48a451138b74fee1f5501aaca1bd80ab6a139
Enes Ozmert <en-es111@hotmail.com>
2023-05-18 11:06:42 +0300

    master pull

--- src/entity/Request.cpp
+++ src/entity/Request.cpp
@@ -133,7 +133,7 @@ std::string Request::getHttpMethodName()
 int Request::getReturnCode()
 {
     return (_returnCode);
-
+}
 std::string Request::getAuthScheme()
 {
     return (this->_authScheme);
