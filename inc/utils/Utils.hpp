da524f6913b908d7ce7741454beb86579c398619
Fatma-Ozturk <fatmabuyukkozturk@gmail.com>
2023-05-26 17:33:42 +0300

    extra color codes

--- inc/utils/Utils.hpp
+++ inc/utils/Utils.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 // Colors
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define RESET "\033[0m"
+#define RED		"\033[31m"
+#define GREEN	"\033[32m"
+#define YELLOW	"\033[33m"
+#define	BLUE	"\033[34m"
+#define	PURPLE	"\033[35m"
+#define	CYAN	"\033[36m"
+#define RESET	"\033[0m"
 
 #include <vector>
 #include <map>
