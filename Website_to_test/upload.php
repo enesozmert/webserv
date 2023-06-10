38cb58dd5d064b1dba8997a6177b20d205d50d9e
enesozmert <en-es111@hotmail.com>
2023-06-11 02:36:45 +0300

    response update

--- Website_to_test/upload.php
+++ Website_to_test/upload.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Veri Gönderme Formu</title>
-</head>
-<body>
-  <form action="#" method="post">
-    <label for="say">Sayı:</label>
-    <input type="text" name="say" id="say"><br>
-
-    <label for="to">Hedef:</label>
-    <input type="text" name="to" id="to"><br>
-
-    <input type="submit" value="Gönder">
-  </form>
-
-  <?php
+<?php
   if(isset($_POST['say'])){
-    $say = htmlspecialchars($_POST['say']);
-    $to = htmlspecialchars($_POST['to']);
-    // $say = $_ENV['say'];
-    // $to = $_ENV['to'];
+    // $say = htmlspecialchars($_POST['say']);
+    // $to = htmlspecialchars($_POST['to']);
+    $say = $_ENV['say'];
+    $to = $_ENV['to'];
 
     echo '>>>>' . $say . " " . $to;
   }
   ?>
-
-</body>
-</html>
-
